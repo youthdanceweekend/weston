7b4f928ff25278d8c8ae03da1b664f45873aebcf
Harris Lapiroff <harris@chromamine.com>
2015-06-02 21:35:41 -0400

    Added autoprefixer.

--- compass/config.rb
+++ compass/config.rb
@@ -1,11 +1,28 @@
 require 'compass/import-once/activate'
+require 'autoprefixer-rails'
 
 http_path = "/"
 css_dir = "../css"
 sass_dir = "sass"
 images_dir = "../images"
 javascripts_dir = "javascripts"
-output_style = :compressed
+output_style = :nested
 relative_assets = true
 line_comments = false
 preferred_syntax = :sass
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  map = file + '.map'
+
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+      from: file,
+      to:   file,
+      map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
+  end
+end
