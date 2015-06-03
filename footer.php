75747a039303bcabea5876ad9b609157c4fa9f8c
Harris Lapiroff <harris@chromamine.com>
2015-06-03 00:43:15 -0400

    Footer refinement.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 			<footer class="site-footer">
 				<div class="container">
 					<div class="row">
-						<div class="col-md-3 col-sm-2">
+						<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 margin-trailer">
 							<?php wp_nav_menu(array(
 								'theme_location' => 'footer1',
 								'depth' => 2,
@@ -9,7 +9,7 @@
 								'menu_class' => 'ydw-nav-list',
 							)); ?>
 						</div>
-						<div class="col-md-3 col-sm-2">
+						<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 margin-trailer">
 							<?php wp_nav_menu(array(
 								'theme_location' => 'footer2',
 								'depth' => 2,
@@ -17,7 +17,8 @@
 								'menu_class' => 'ydw-nav-list',
 							)); ?>
 						</div>
-						<div class="col-md-3 col-sm-2">
+						<div class="clearfix hidden-md hidden-lg"></div>
+						<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 margin-trailer">
 							<?php wp_nav_menu(array(
 								'theme_location' => 'footer3',
 								'depth' => 2,
@@ -25,7 +26,7 @@
 								'menu_class' => 'ydw-nav-list',
 							)); ?>
 						</div>
-						<div class="col-md-3 col-sm-2">
+						<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 margin-trailer">
 							<?php wp_nav_menu(array(
 								'theme_location' => 'footer4',
 								'depth' => 2,
