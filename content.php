17382e30e15970a1773a2a326427836237736870
Harris Lapiroff <harris@chromamine.com>
2015-06-08 18:18:20 -0400

    Updates to post template.

--- content.php
+++ content.php
@@ -11,48 +11,41 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() && ! post_password_required() && ! is_attachment() ) : ?>
-		<div class="entry-thumbnail">
-			<?php the_post_thumbnail(); ?>
-		</div>
-		<?php endif; ?>
-
+	<header>
 		<?php if ( is_single() ) : ?>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
 		<?php else : ?>
-		<h1 class="entry-title">
-			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-		</h1>
+			<h1 class="entry-title">
+				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+			</h1>
 		<?php endif; // is_single() ?>
 
-		<div class="entry-meta">
-			<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
+		<div>
+			<a href="<?= get_edit_post_link(); ?>"></a>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
 	<?php else : ?>
-	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentythirteen' ),
-				the_title( '<span class="screen-reader-text">', '</span>', false )
-			) );
+		<div class="entry-content">
+			<?php
+				the_content( sprintf(
+					'Continue reading %s <span class="meta-nav">&rarr;</span>'
+					the_title( '<span class="screen-reader-text">', '</span>', false )
+				) );
 
-			wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) );
-		?>
-	</div><!-- .entry-content -->
+				wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">Pages:</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) );
+			?>
+		</div><!-- .entry-content -->
 	<?php endif; ?>
 
 	<footer class="entry-meta">
 		<?php if ( comments_open() && ! is_single() ) : ?>
 			<div class="comments-link">
-				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'twentythirteen' ) . '</span>', __( 'One comment so far', 'twentythirteen' ), __( 'View all % comments', 'twentythirteen' ) ); ?>
+				<?php comments_popup_link( '<span class="leave-reply">Leave a comment</span>', 'One comment so far', 'View all % comments'); ?>
 			</div><!-- .comments-link -->
 		<?php endif; // comments_open() ?>
 
