e3dcb006dfc22144b0146620816b7808b62d07d2
Harris Lapiroff <harris@chromamine.com>
2015-06-08 18:18:59 -0400

    Fix.

--- content.php
+++ content.php
@@ -33,7 +33,7 @@
 		<div class="entry-content">
 			<?php
 				the_content( sprintf(
-					'Continue reading %s <span class="meta-nav">&rarr;</span>'
+					'Continue reading %s <span class="meta-nav">&rarr;</span>',
 					the_title( '<span class="screen-reader-text">', '</span>', false )
 				) );
 
