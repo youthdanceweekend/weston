3db265dc48e9c77a57633f9384428fec1036384f
Harris Lapiroff <harris@chromamine.com>
2018-05-04 02:30:27 -0400

    HTTPS-readiness

--- functions.php
+++ functions.php
@@ -48,8 +48,8 @@ function weston_scripts() {
 
 	// Adobe Edge Fonts: Montserrat, Voltaire, Lato
 	wp_enqueue_script('weston-fonts', '//use.edgefonts.net/montserrat;voltaire;lato:n3,i3,n4,i4.js', array(), '20150315', false);
-	wp_enqueue_script('weston-jquery', 'http://code.jquery.com/jquery-1.11.3.min.js', array(), '1.11.3', false);
-	wp_enqueue_script('weston-bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array(), '3.3.4', false);
+	wp_enqueue_script('weston-jquery', '//code.jquery.com/jquery-1.11.3.min.js', array(), '1.11.3', false);
+	wp_enqueue_script('weston-bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array(), '3.3.4', false);
 	wp_enqueue_script('weston-sliding-nav', get_template_directory_uri() . '/js/weston.slidingNav.js', array(), '20150601', false);
 	wp_enqueue_script('weston-modernizr', get_template_directory_uri() . '/lib/modernizr.js', array(), '20150602', false);
 }
